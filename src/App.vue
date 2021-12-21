9260fbc41fc898deb530752e23e5a17a62b1eb2a
Artur Książek <arthur.ksiazek@gmail.com>
2021-12-21 19:49:25 +0100

    Preliminary html/css draft of board

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="bg-gray-200 h-screen">
+    <div class="grid grid-cols-12">
+      <div class="col-span-1 h-screen bg-white p-4">
+        <div
+          class="w-full border border-gray-300 rounded-md px-3 py-2 text-gray-500 mb-4 text-sm"
+        >
+          Select board
+        </div>
+
+        <div>
+          <router-link to="/">Home</router-link> |
+          <router-link to="/board">Board</router-link>
+        </div>
+      </div>
+      <div class="col-span-10 p-10">
+        <router-view />
+      </div>
+    </div>
   </div>
-  <router-view />
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
---
+++ src/views/Board.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="grid gap-8 grid-cols-5">
+    <div class="" v-for="column in columns" :key="column.title">
+      <h2 class="text-sm text-gray-500">{{ column.title }}</h2>
+      <div class="mt-4">
+        <div
+          v-for="item in column.items"
+          :key="item.title"
+          class="bg-white text-xs rounded-md border-l-4 border-blue-500 overflow-hidden mt-3 shadow-md"
+        >
+          <div class="py-3 px-4 text-gray-600">
+            {{ item.title }}
+          </div>
+          <div class="bg-gray-100 py-2 px-4 flex justify-between items-center">
+            <span class="text-gray-400">#1337</span>
+            <span
+              class="rounded-full bg-blue-300 h-6 w-6 flex items-center justify-center text-blue-600"
+            >
+              AK
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Board",
+  components: {},
+  setup() {
+    const columns = [
+      {
+        title: "Ready for development",
+        items: [
+          {
+            title: "The list of repositories",
+          },
+          {
+            title: "The function to update the list of repositories",
+          },
+          {
+            title:
+              "Returning both the list and the function so they are accessible by other component options",
+          },
+          {
+            title: "The list of repositories",
+          },
+          {
+            title: "The function to update the list of repositories",
+          },
+          {
+            title:
+              "Returning both the list and the function so they are accessible by other component options",
+          },
+        ],
+      },
+      {
+        title: "In development",
+        items: [],
+      },
+      {
+        title: "Ready for review",
+        items: [],
+      },
+      {
+        title: "Ready for deploy",
+        items: [],
+      },
+      {
+        title: "Completed",
+        items: [],
+      },
+    ];
+
+    return {
+      columns,
+    };
+  },
+});
+</script>
